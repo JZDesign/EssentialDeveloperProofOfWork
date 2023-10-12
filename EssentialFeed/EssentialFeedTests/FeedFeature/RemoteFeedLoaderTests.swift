9434ad83ee7f35438a460ee7b3a7b22be9940be3
Jacob Rakidzich <jacob.rakidzich@daveramsey.com>
2023-10-12 13:14:57 -0500

    Inject httpclient and test that load will invoke the client

--- EssentialFeed/EssentialFeedTests/FeedFeature/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/FeedFeature/RemoteFeedLoaderTests.swift
@@ -7,19 +7,48 @@
 
 import XCTest
 
-class HTTPClient {
-    var requestedURL: URL?
+protocol HTTPClient {
+    func get(from url: URL)
 }
 
-class RemoteFeedLoader { }
+
+class HTTPClientSpy: HTTPClient {
+    var requestedURL: URL?
+    
+    func get(from url: URL) {
+        requestedURL = url
+    }
+}
+
+
+class RemoteFeedLoader {
+    private let client: HTTPClient
+    
+    init(client: HTTPClient) {
+        self.client = client
+    }
+    
+    func load() {
+        client.get(from: URL(string: "http://test.com")!)
+    }
+}
 
 final class RemoteFeedLoaderTests: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL() {
-        let client = HTTPClient()
-        _ = RemoteFeedLoader()
+        let client = HTTPClientSpy()
+        _ = RemoteFeedLoader(client: client)
         
         XCTAssertNil(client.requestedURL)
+    }
+    
+    func test_load_requestsDataFromURL() {
+        let client = HTTPClientSpy()
+        let sut = RemoteFeedLoader(client: client)
+        
+        sut.load()
+        
+        XCTAssertNotNil(client.requestedURL)
     }
 
 }
