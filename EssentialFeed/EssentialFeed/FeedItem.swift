1914e92a8771e4187ec86569e10e68d20c57802f
Jacob Rakidzich <jacob.rakidzich@daveramsey.com>
2023-10-12 12:37:55 -0500

    add feed item

---
+++ EssentialFeed/EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Jacob Rakidzich on 10/12/23.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
