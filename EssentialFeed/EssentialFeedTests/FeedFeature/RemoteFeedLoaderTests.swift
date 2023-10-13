4c28b230e3b976188538c6d9c7d95fe18bbb8fed
Jacob Rakidzich <jacob.rakidzich@daveramsey.com>
2023-10-13 13:35:20 -0500

    clean up tests with helper method

--- EssentialFeed/EssentialFeedTests/FeedFeature/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/FeedFeature/RemoteFeedLoaderTests.swift
@@ -90,24 +90,27 @@ final class RemoteFeedLoaderTests: XCTestCase {
     
     func test_load_deliversItemsOn200WithJSONList() {
         let (sut, client) = makeSUT()
-        let item1 = makeItem(description: "1", location: "1", imageURL: URL(string: "http://test1.com")!)
+        let item1 = makeItem(imageURL: URL(string: "http://test1.com")!)
         let item2 = makeItem(description: "2", location: "2", imageURL: URL(string: "http://test2.com")!)
         
-        let jsonObject = ["items": [ item1.json, item2.json ]]
-        
-        expect(sut, toCompleteWith: .success([item1.item, item2.item])) {
-            client.complete(with: 200, data: try! JSONSerialization.data(withJSONObject: jsonObject))
+        expect(sut, toCompleteWith: .success([item1.model, item2.model])) {
+            let json = makeItemsJson(items: [item1.json, item2.json])
+            client.complete(with: 200, data: try! JSONSerialization.data(withJSONObject: json))
         }
     }
     
     // MARK: - Helpers
     
-    func makeItem(id: UUID = .init(), description: String? = nil, location: String? = nil, imageURL: URL) -> (item: FeedItem, json: [String : Encodable]) {
+    private func makeItem(id: UUID = .init(), description: String? = nil, location: String? = nil, imageURL: URL) -> (model: FeedItem, json: [String : Encodable]) {
         let item = FeedItem(id: id, description: description, location: location, imageURL: imageURL)
         return (item, jsonItem(for: item))
     }
     
-    func jsonItem(for item: FeedItem) -> [String : Encodable] {
+    private func makeItemsJson(items: [[String : Any]]) -> [String : Any] {
+        ["items": items]
+    }
+    
+    private func jsonItem(for item: FeedItem) -> [String : Encodable] {
         [
             "id": item.id.uuidString,
             "description": item.description,
@@ -116,7 +119,7 @@ final class RemoteFeedLoaderTests: XCTestCase {
         ]
     }
     
-    func expect(
+    private func expect(
         _ sut: RemoteFeedLoader,
         toCompleteWith result: RemoteFeedLoaderResult,
         file: StaticString = #file,
@@ -131,7 +134,7 @@ final class RemoteFeedLoaderTests: XCTestCase {
         XCTAssertEqual(results, [result], file: file, line: line)
     }
     
-    func makeSUT(url: URL = URL(string: "http://test.com")!) -> (RemoteFeedLoader, HTTPClientSpy) {
+    private func makeSUT(url: URL = URL(string: "http://test.com")!) -> (RemoteFeedLoader, HTTPClientSpy) {
         let client = HTTPClientSpy()
         let sut = RemoteFeedLoader(url: url, client: client)
         return (sut, client)
