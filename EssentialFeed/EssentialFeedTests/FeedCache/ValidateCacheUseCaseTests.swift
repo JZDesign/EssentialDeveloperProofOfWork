5035b0b46058f8f031618976513f7bfc01f4f248
Jacob Rakidzich <jacob.rakidzich@daveramsey.com>
2023-10-19 08:46:59 -0500

    remove unused helper method

--- EssentialFeed/EssentialFeedTests/FeedCache/ValidateCacheUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/FeedCache/ValidateCacheUseCaseTests.swift
@@ -90,35 +90,4 @@ final class ValidateCacheUseCaseTests: XCTestCase {
         let loader = createAndTrackMemoryLeaks(LocalFeedLoader(store:  store, currentDate: currentDate), file: file, line: line)
         return (loader, store)
     }
-    
-    func expect(
-        _ sut: LocalFeedLoader,
-        toCompleteWith expectedResult: LoadFeedResult,
-        file: StaticString = #file,
-        line: UInt = #line,
-        when action: () -> Void
-    ) {
-        
-        let expectation = expectation(description: #function)
-        
-        sut.load { recievedResult in
-            switch (recievedResult, expectedResult) {
-            case let (.success(values), .success(expectedValues)):
-                XCTAssertEqual(values, expectedValues, file: file, line: line)
-            case let (.failure(error), .failure(expectedError)):
-                XCTAssertEqual(
-                    (error as? EquatableError)!,
-                    (expectedError as? EquatableError)!,
-                    file: file,
-                    line: line
-                )
-            default:
-                XCTFail("Expected \(expectedResult) got \(recievedResult) instead")
-            }
-            expectation.fulfill()
-        }
-        
-        action()
-        wait(for: [expectation])
-    }
 }
