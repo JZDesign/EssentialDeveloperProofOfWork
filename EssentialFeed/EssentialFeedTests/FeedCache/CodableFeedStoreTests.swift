2785588f721d385dcacf6d8eb816c5c78a4db080
Jacob Rakidzich <jacob.rakidzich@daveramsey.com>
2023-10-19 10:32:58 -0500

    test_retrieve_hasNoSideEffectsOnFailure

--- EssentialFeed/EssentialFeedTests/FeedCache/CodableFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/FeedCache/CodableFeedStoreTests.swift
@@ -111,6 +111,15 @@ class CodableFeedStoreTests: XCTestCase {
         expect(sut, toRetrieve: .failure(EquatableError()))
     }
     
+    func test_retrieve_hasNoSideEffectsOnFailure() {
+        let storeURL = testSpecificStoreURL()
+        let sut = makeSUT(storeURL: storeURL)
+        
+        try! "invalid data".write(to: storeURL, atomically: false, encoding: .utf8)
+        
+        expect(sut, toRetrieveTwice: .failure(anyNSError()))
+    }
+    
     // Helpers
     
     private func testSpecificStoreURL() -> URL {
