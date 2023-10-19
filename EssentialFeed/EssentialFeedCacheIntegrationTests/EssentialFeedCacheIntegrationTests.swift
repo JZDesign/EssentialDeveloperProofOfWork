b097911d192f10eb2e53deb8c5a9dd4a9877fe29
Jacob Rakidzich <jacob.rakidzich@daveramsey.com>
2023-10-19 14:51:47 -0500

    Create new target

---
+++ EssentialFeed/EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -0,0 +1,15 @@
+//
+//  EssentialFeedCacheIntegrationTests.swift
+//  EssentialFeedCacheIntegrationTests
+//
+//  Created by Jacob Rakidzich on 10/19/23.
+//
+
+import XCTest
+
+final class EssentialFeedCacheIntegrationTests: XCTestCase {
+
+    func test_() throws {
+        
+    }
+}
