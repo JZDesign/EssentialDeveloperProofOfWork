1aa80cc1b0e6421e7568d1786f1c1f2e36cd420e
Jacob Rakidzich <jacob.rakidzich@daveramsey.com>
2023-10-20 09:43:44 -0500

    add empty project for prototype

---
+++ Prototype/Prototype/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Prototype
+//
+//  Created by Jacob Rakidzich on 10/20/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
