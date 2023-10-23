962c549bedf9555293efcdb1cd4dcd1c60dfe88f
Jacob Rakidzich <jacob.rakidzich@daveramsey.com>
2023-10-23 05:51:32 -0500

    test_viewDidLoad_hidesLoadingIndicatorOnLoaderCompletion

--- EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
+++ EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -38,7 +38,9 @@ final class FeedViewController: UITableViewController {
     @objc private func load() {
         refreshControl?.beginRefreshing()
 
-        loader?.load { _ in }
+        loader?.load { [weak self] _ in
+            self?.refreshControl?.endRefreshing()
+        }
     }
 }
 
@@ -79,6 +81,15 @@ final class FeedViewControllerTests: XCTestCase {
         XCTAssertEqual(sut.refreshControl?.isRefreshing, true)
     }
 
+    func test_viewDidLoad_hidesLoadingIndicatorOnLoaderCompletion() {
+        let (sut, loader) = makeSUT()
+        
+        sut.simulateAppearance()
+        loader.completeFeedLoading()
+        
+        XCTAssertEqual(sut.refreshControl?.isRefreshing, false)
+    }
+
     // MARK: - Helpers
 
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: FeedViewController, loader: LoaderSpy) {
@@ -88,10 +99,18 @@ final class FeedViewControllerTests: XCTestCase {
     }
     
     class LoaderSpy: FeedLoader {
-        private(set) var loadCallCount: Int = 0
+        var loadCallCount: Int {
+            completions.count
+        }
+
+        private var completions = [(FeedLoader.Result) -> Void]()
+
+        func load(completion: @escaping (FeedLoader.Result) -> Void) {
+            completions.append(completion)
+        }
         
-        func load(completion: @escaping (Result<[EssentialFeed.FeedImage], Error>) -> Void) {
-            loadCallCount += 1
+        func completeFeedLoading(with images: [FeedImage] = [], at index: Int = 0) {
+            completions[index](.success(images))
         }
     }
 }
