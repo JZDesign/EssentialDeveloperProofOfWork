7d76f1d876a66521a8f85ff40ab975e442b2baf8
Jacob Rakidzich <jacob.rakidzich@daveramsey.com>
2023-10-24 09:16:13 -0500

    FeedErrorViewModel to its own file

---
+++ EssentialFeed/EssentialFeediOS/FeedUI/Presentation/ViewModels/FeedErrorViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedErrorViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Jacob Rakidzich on 10/24/23.
+//
+
+import Foundation
+
+struct FeedErrorViewModel {
+    let message: String?
+}
