91a28f853b90ade276137eef78025e06b33c38e5
Jacob Rakidzich <jacob.rakidzich@daveramsey.com>
2023-10-24 09:06:21 -0500

    ErrorView to its own file

---
+++ EssentialFeed/EssentialFeediOS/FeedUI/Views/ErrorView.swift
@@ -0,0 +1,12 @@
+//
+//  ErrorView.swift
+//  EssentialFeediOS
+//
+//  Created by Jacob Rakidzich on 10/24/23.
+//
+
+import UIKit
+
+public final class ErrorView: UIView {
+    public var message: String?
+}
