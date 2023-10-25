bfe25da46f829e178f9c4532e94fb116848aaaae
Jacob Rakidzich <jacob.rakidzich@daveramsey.com>
2023-10-24 21:17:14 -0500

    Add app + workspace

---
+++ EssentialApp/EssentialApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EssentialApp
+//
+//  Created by Jacob Rakidzich on 10/24/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
