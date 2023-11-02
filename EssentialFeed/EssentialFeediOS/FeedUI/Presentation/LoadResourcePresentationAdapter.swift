74e93770fa2d14e7171709fe84c06cc6318b5c9f
Jacob Rakidzich <jacob.rakidzich@daveramsey.com>
2023-11-02 10:17:37 -0500

    Make Presentation Adapter generic so it can be reused

--- EssentialFeed/EssentialFeediOS/FeedUI/Presentation/LoadResourcePresentationAdapter.swift
+++ EssentialFeed/EssentialFeediOS/FeedUI/Presentation/LoadResourcePresentationAdapter.swift
@@ -1,5 +1,5 @@
 //
-//  FeedLoaderPresentationAdapter.swift
+//  LoadResourcePresentationAdapter.swift
 //  EssentialFeediOS
 //
 //  Created by Jacob Rakidzich on 10/23/23.
@@ -8,18 +8,18 @@
 import Combine
 import EssentialFeed
 
-final class FeedLoaderPresentationAdapter: FeedViewControllerDelegate {
-    private let feedLoader: () -> AnyPublisher<[FeedImage], Error>
+final class LoadResourcePresentationAdapter<Resource, View: ResourceView> {
+    private let loader: () -> AnyPublisher<Resource, Error>
     private var cancellable: Cancellable?
-    var presenter: LoadResourcePresenter<[FeedImage], FeedViewAdapter>?
-
-    init(feedLoader: @escaping () ->  AnyPublisher<[FeedImage], Error>) {
-        self.feedLoader = feedLoader
+    var presenter: LoadResourcePresenter<Resource, View>?
+    
+    init(loader: @escaping () -> AnyPublisher<Resource, Error>) {
+        self.loader = loader
     }
-
-    func didRequestFeedRefresh() {
+    
+    func loadResource() {
         presenter?.didStartLoading()
-        cancellable = feedLoader()
+        cancellable = loader()
             .dispatchOnMainQueue()
             .sink(
                 receiveCompletion: { [weak self] completion in
@@ -29,12 +29,20 @@ final class FeedLoaderPresentationAdapter: FeedViewControllerDelegate {
                     case let .failure(error):
                         self?.presenter?.didFinishLoading(with: error)
                     }
-                }, receiveValue: { [weak self] feed in
-                    self?.presenter?.didFinishLoading(with: feed)
+                }, receiveValue: { [weak self] resource in
+                    self?.presenter?.didFinishLoading(with: resource)
                 }
             )
     }
 }
+
+extension LoadResourcePresentationAdapter: FeedViewControllerDelegate {
+    
+    func didRequestFeedRefresh() {
+        loadResource()
+    }
+}
+
 
 public extension LocalFeedLoader {
     typealias Publisher = AnyPublisher<[FeedImage], Error>
