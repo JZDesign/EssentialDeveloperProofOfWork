9b848975234c8f7467e9c5c4e69a7e1425c43543
Jacob Rakidzich <jacob.rakidzich@daveramsey.com>
2023-11-02 10:13:09 -0500

    remove feed presenter logic that was unused

--- EssentialFeed/EssentialFeediOS/FeedUI/Presentation/Presenters/FeedPresenter.swift
+++ EssentialFeed/EssentialFeediOS/FeedUI/Presentation/Presenters/FeedPresenter.swift
@@ -7,17 +7,9 @@
 
 import EssentialFeed
 
-
-public protocol FeedView {
-    func display(_ viewModel: FeedViewModel)
-}
-
 public final class FeedPresenter {
+    private init() { }
 
-    private let feedView: FeedView
-    private let loadingView: ResourceLoadingView
-    private let errorView: ResourceErrorView
-    
     static var title: String {
         NSLocalizedString(
             "FEED_VIEW_TITLE",
@@ -25,36 +17,6 @@ public final class FeedPresenter {
             bundle: Bundle(for: FeedPresenter.self),
             comment: "Title for the feed view"
         )
-    }
-    
-    private var feedLoadError: String {
-        NSLocalizedString(
-            "GENERIC_CONNECTION_ERROR",
-            tableName: "Shared",
-            bundle: Bundle(for: FeedPresenter.self),
-            comment: "Error message displayed when we can't load the image feed from the server"
-        )
-    }
-
-    init(feedView: FeedView, loadingView: ResourceLoadingView, errorView: ResourceErrorView) {
-        self.feedView = feedView
-        self.loadingView = loadingView
-        self.errorView = errorView
-    }
-    
-    func didStartLoadingFeed() {
-        errorView.display(.noError)
-        loadingView.display(ResourceLoadingViewModel(isLoading: true))
-    }
-    
-    func didFinishLoadingFeed(with feed: [FeedImage]) {
-        feedView.display(Self.map(feed))
-        loadingView.display(ResourceLoadingViewModel(isLoading: false))
-    }
-
-    func didFinishLoadingFeed(with error: Error) {
-        errorView.display(.error(message: feedLoadError))
-        loadingView.display(ResourceLoadingViewModel(isLoading: false))
     }
     
     public static func map(_ feed: [FeedImage]) -> FeedViewModel {
