efe179e65fdacf9f8ca5de53d55102f2a8d95efe
Jacob Rakidzich <jacob.rakidzich@daveramsey.com>
2023-11-06 09:59:41 -0600

    Deprecate async APIs in favor of new sync APIs

---
+++ EssentialFeed/EssentialFeed/FeedAPI/FeedImageDataStore.swift
@@ -0,0 +1,18 @@
+//
+//  RemoteFeedImageDataLoader.swift
+//  EssentialFeed
+//
+//  Created by Jacob Rakidzich on 10/24/23.
+//
+
+import Foundation
+
+public protocol FeedImageDataLoaderTask {
+    func cancel()
+}
+
+public protocol FeedImageDataStore {
+    typealias Result = Swift.Result<Data, Error>
+    
+    func loadImageData(from url: URL, completion: @escaping (Result) -> Void) -> FeedImageDataLoaderTask
+}
