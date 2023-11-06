5efd5cdcbeb7d141f661d5b05123aa41b876914d
Jacob Rakidzich <jacob.rakidzich@daveramsey.com>
2023-11-06 11:28:14 -0600

    make null store sync

--- EssentialApp/EssentialApp/NullStore.swift
+++ EssentialApp/EssentialApp/NullStore.swift
@@ -8,24 +8,11 @@
 import Foundation
 import EssentialFeed
 
-class NullStore: FeedStore & FeedImageDataStore {
-    func deleteCachedFeed(completion: @escaping DeletionCompletion) {
-        completion(.success(()))
-    }
-
-    func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
-        completion(.success(()))
-    }
-
-    func retrieve(completion: @escaping RetrievalCompletion) {
-        completion(.success(.none))
-    }
-
-    func insert(_ data: Data, for url: URL, completion: @escaping (InsertionResult) -> Void) {
-        completion(.success(()))
-    }
-
-    func retrieve(dataForURL url: URL, completion: @escaping (FeedImageDataStore.RetrievalResult) -> Void) {
-        completion(.success(.none))
-    }
+class NullStore: FeedStore, FeedImageDataStore {
+    func retrieve() throws -> EssentialFeed.CachedFeed? { nil }
+    func insert(_ feed: [EssentialFeed.LocalFeedImage], timestamp: Date) throws {}
+    func deleteCachedFeed() throws {}
+    
+    func insert(_ data: Data, for url: URL) throws {}
+    func retrieve(dataForURL url: URL) throws -> Data? { .none }
 }
